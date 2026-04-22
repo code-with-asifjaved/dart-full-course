73da4bc5bf419891653f0f57c79878deb09ad031
code-with-asifjaved <asif3211@gmail.com>
2026-04-22 09:02:57 -0700

    Create lists_practice.dart

---
+++ lists_practice.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> students = ['Asif','Wasi','Amir',
+                           'Sidra','Fiza'];
+  students.add('Zain');
+  print(students);
+  print(students[0
+                ]);
+  
+}
