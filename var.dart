066de3778e5f9065cf4023b0b8a8421022449f08
code-with-asifjaved <asif3211@gmail.com>
2026-04-22 06:59:31 -0700

    Create var.dart

---
+++ var.dart
@@ -0,0 +1,23 @@
+void main() {
+  int number1 = 2;
+  int number2 = 2;
+  int sum1;
+  sum1 = number1+number2;
+  print(sum1);
+  String name = 'Asif jave';
+  print(name);
+  //you can also write true here
+  bool isChaiArrived = false;
+  print(isChaiArrived);
+  double height = 5.1;
+  // we have operators like + - * / %
+  print(number1+number2);
+  print(number1+height);
+  double sum = number1+height;
+  print(sum);
+  var age = 12;
+  //this is called inference (value daine ki bjaye valye infer kerna) yhan veriable int number = 12 ki jaga var age = 12 infer ho k chala hai. 
+  //this is invalid now age = "asif" kren gay to yeh error day ga. integer walae box main String dal day gay to ye error day day ga . q k "asif" aik sting and jbko "12" integer hai.
+  print(age);
+  
+}
