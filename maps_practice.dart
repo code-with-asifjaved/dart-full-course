06d37c14d29a8bd964fc5a1538e14fa06d24f96c
code-with-asifjaved <asif3211@gmail.com>
2026-04-22 19:40:46 -0700

    Create maps_practice.dart

---
+++ maps_practice.dart
@@ -0,0 +1,11 @@
+void main(){
+  Map<String, String> user = {
+    "Name": "Muhammad Asif Javed",
+    "Skill": "Graphic Design",
+    "City": "Faisalabd"
+   
+  };
+  print(user["Name"]);
+  print(user["City"]);
+  
+}
